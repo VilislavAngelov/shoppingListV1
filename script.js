f11aff4b33849aae779d7f1b2afcbf03a10101f2
Vilislav <vilislavangelov@gmail.com>
2024-10-21 07:32:11 +0300

    Finished the exercise by making a form and typing
    whatever I want to be added to the list.
    It works by invoking a js function on button click
    pretty fun project.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 const uList = document.querySelector("ul");
 const inputField = document.querySelector("input");
 const btn = document.querySelector("button");
+
+
+btn.addEventListener ("click", () => {
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const btnDelete = document.createElement("button");
+
+
+    li.appendChild(span);
+    li.appendChild(btnDelete);
+
+    span.textContent = inputField.value;
+    btnDelete.textContent = "Delete";
+
+    uList.appendChild(li);
+
+    inputField.value = '';
+
+    btnDelete.addEventListener( "click", () => {
+        btnDelete.parentElement.remove();
+    })
+
+    inputField.focus();
+})
